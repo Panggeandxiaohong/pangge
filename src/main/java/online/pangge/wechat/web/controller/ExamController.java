1a7389a65145944f2981aa86cd335b8ca5da693c
jimmy <m13427598892@163.com>
2017-09-23 15:08:44 +0800

    subject redis

--- src/main/java/online/pangge/wechat/web/controller/ExamController.java
+++ src/main/java/online/pangge/wechat/web/controller/ExamController.java
@@ -2,13 +2,14 @@ package online.pangge.wechat.web.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ExamController {
     @RequestMapping("/exam.do")
-    public String exam(){
-
+    public String exam(String src, Model model){
+        model.addAttribute("src",src);
         return "wechat/subjectView";
     }
 }
--- src/main/webapp/WEB-INF/views/wechat/subjectView.jsp
+++ src/main/webapp/WEB-INF/views/wechat/subjectView.jsp
@@ -12,7 +12,7 @@
 </head>
 <body>
 <video width="320" height="240" controls="controls">
-    <source src="${subject.url}" type="video/mp4" />
+    <source src="${src}" type="video/mp4" />
     Your browser does not support the video tag.
 </video>
 </body>
