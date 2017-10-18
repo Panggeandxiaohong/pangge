7fc9f207aec8dcc30fd6c85257f7ca985f2b9536
jimmy <m13427598892@163.com>
2017-10-18 20:41:19 +0800

    shell

---
+++ src/main/java/online/pangge/helloShell.java
@@ -0,0 +1,12 @@
+package online.pangge;
+
+import java.util.Date;
+
+/**
+ * Created by jie34 on 2017/10/18.
+ */
+public class helloShell {
+    public static void main(String[] args){
+        System.out.println("time = "+new Date().toLocaleString());
+    }
+}
